766eae7818a324d265265a201d375f519f5044f2
vingleo <vingleo@vingleodemacbook-pro.local>
2017-03-30 22:49:58 +0800

    Initial Commit

---
+++ Pockedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pockedex3
+//
+//  Created by vingleo on 17/3/30.
+//  Copyright © 2017年 vingleo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
